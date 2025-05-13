2ab513c955b520a72d678836f4621e314a48a548
budsonjelmont <budsonjelmont@gmail.com>
2025-05-13 13:25:27 -0400

    wip

--- lcsm/lcsm.py
+++ lcsm/lcsm.py
@@ -1,8 +1,8 @@
 # Problem
 
-# A common substring of a collection of strings is a substring of every member of the collection. 
-# We say that a common substring is a longest common substring if there does not exist a longer common substring. 
-# For example, "CG" is a common substring of "ACGTACGT" and "AACCGTATA", but it is not as long as possible; 
+# A common substring of a collection of strings is a substring of every member of the collection.
+# We say that a common substring is a longest common substring if there does not exist a longer common substring.
+# For example, "CG" is a common substring of "ACGTACGT" and "AACCGTATA", but it is not as long as possible;
 # in this case, "CGTA" is a longest common substring of "ACGTACGT" and "AACCGTATA".
 
 # Note that the longest common substring is not necessarily unique; for a simple example, "AA" and "CC" are both longest common substrings of "AACC" and "CCAA".
@@ -29,36 +29,54 @@
 import sys
 from Bio import SeqIO
 
-
 infile = sys.argv[1]
 
 lendict = {}
 occurdict = {}
 
-# TOO SLOW
+seqs = {}
 
 with open(infile) as handle:
     for index, record in enumerate(SeqIO.parse(handle, "fasta")):
         # print(record.id)
         # print(record.name)
-        nseqs += 1
-        seq = record.seq
-        minlen = len(seq) if len(seq) < minlen or minlen==0 else minlen
-        for pos in range(0, len(seq)):
-            iter_substrs(index, seq, '', pos, 0)
+        seqs[index] = record.seq
 
-# print(lendict)
-# print(occurdict)
+posixs_scores = {}
 
-found = False
-for length in range(minlen, 0,-1):
-    for subseq in lendict[length]:
-        if len(occurdict[subseq])==nseqs:
-            print(subseq)
-            found = True
+
+def iter_seqs(postuple):
+    print(f"now on postuple: {postuple}")
+    for i in range(len(postuple)):
+        if postuple[i] + 1 < len(seqs[i]):
+            print(
+                f"queueing up next postuple: {tuple(postuple[j] if j!=i else postuple[j]+1 for j in range(len(postuple)))}"
+            )
+            iter_seqs(
+                tuple(
+                    postuple[j] if j != i else postuple[j] + 1
+                    for j in range(len(postuple))
+                )
+            )
+    lastchar = None
+    allmatch = True
+    for i in range(len(postuple)):
+        currentchar = seqs[i][postuple[i]]
+        if not lastchar:
+            lastchar = currentchar
+        elif lastchar == currentchar:
+            continue
+        else:
+            allmatch = False
             break
-    if found:
-        break
+    if allmatch:
+        if False in [i - 1 >= 0 for i in postuple]:
+            score = 1
+        else:
+            score = posixs_scores[tuple(i - 1 for i in postuple)] + 1
+    else:
+        score = 0
+    posixs_scores[postuple] = score
 
-if not found:
-    print(None)
+
+iter_seqs(tuple(0 for i in range(len(seqs))))
